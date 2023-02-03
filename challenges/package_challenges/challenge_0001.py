4caa51623432e23723b2e5954b9b8e491024e8b5
manojsarathybigdata <p.manojsarathy@gmail.com>
2023-02-04 04:12:18 +0530

    Committing the challenge 1 code

---
+++ challenges/package_challenges/challenge_0001.py
@@ -0,0 +1,12 @@
+"""
+Capital indexes
+Write a function named capital_indexes. The function takes a single parameter, which is a string. Your function should return a list of all the indexes in the string that have capital letters.
+
+For example, calling capital_indexes("HeLlO") should return the list [0, 2, 4].
+"""
+import re
+def capital_indexes(test_string):
+    return ([i for i, j in enumerate(test_string) if re.match('[A-Z]',j)])
+
+final = capital_indexes("ManoM")
+print (final)
